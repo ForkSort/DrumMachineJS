7d764695255ac489ec4ad1422830b01b882571e0
ForkSort <95124571+ForkSort@users.noreply.github.com>
2022-02-13 01:08:37 +0100

    Change: Use Webaudio API instead

--- js/DrumMachine.js
+++ js/DrumMachine.js
@@ -1,30 +1,64 @@
 import Drum from "./Drum.js";
 /**
- * DrumMachine class, it just creates DOM-elements for each drum-instance and instantiates the Drum-instances.
+ * DrumMachine class, creates DOM-elements for each drum-instance and instantiates the Drum-instances.
+ * @param {*} props {parent: parent-DOM element to render at, audioContext: the audioContext instance, audioOutput: audio output destination, drumKit: array[{soundUrl: url, id: a name, key: keyboard-mapping key}]}
  */
 export default class DrumMachine {
     constructor(props) {
-        const {element, drumKit} = props;
-        this.element = element;
-        this.drums = [];
+        const {parent, audioContext, audioOutput, drumKit} = props;
 
-        /* create a document-fragment for the drums, then loop through the drumkit props, create the DOM elements, and instantiate Drum-instances */
-        const fragment = document.createDocumentFragment();
-        for (const drum of drumKit.drums) {
+        this.drums = {};
+        this.audioContext = audioContext;
+        this.primaryGainControl = this.audioContext.createGain();
+        this.primaryGainControl.gain.setValueAtTime(0.8, 0);
+        this.primaryGainControl.connect(audioOutput);
+
+        /* create the drum-machine root element */
+        const drumMachineElement = this.createElement(
+            "div", {className: "drum-machine"}, [
+                ["div", {}, [
+                    ["h1", {textContent: "DRUM"}, [
+                        ["br", {}], 
+                        ["span", {textContent: "MACHINE"}]
+                    ]],
+                    //["canvas", {id: "oscilloscope"}]
+                ]]
+            ]
+        );
+        /* loop through the drumkit, instantiate the drum-instances and attach eventlisteners*/
+        for (const drum of drumKit) {
+            
             const drumElement = this.createElement(
                 "button", {}, [
                     ["h3", {textContent: drum.id}],
                     ["p", {textContent: "HOTKEY: "}, [
-                        ["span", {textContent: drum.key}]]
-                    ]
+                        ["span", {textContent: drum.key}]
+                    ]]
                 ]
             );
-            this.drums.push(new Drum({
-                element: drumElement, soundFolder: drumKit.soundFolder, fileName: drum.fileName, key: drum.key
-            }));
-            fragment.appendChild(drumElement);
+            this.drums[drum.key] = new Drum({
+                element: drumElement,
+                audioContext: this.audioContext, 
+                audioOutput: this.primaryGainControl, 
+                soundUrl: drum.soundUrl
+            });
+            drumElement.addEventListener('touchstart', (event) => {
+                this.drums[drum.key].play();
+                event.preventDefault();
+            });
+            drumElement.addEventListener('mousedown', (event) => {
+                this.drums[drum.key].play();
+                event.preventDefault();
+            });
+            drumMachineElement.appendChild(drumElement);
         }
-        this.element.appendChild(fragment);
+        parent.appendChild(drumMachineElement);
+
+        document.addEventListener('keydown', (event) => {
+            if (this.drums[event.key] === undefined || event.repeat) return;
+            this.drums[event.key].play();
+            event.preventDefault();
+        });  
     }
     /**
      * Parses parameters through document.createElement, and returns a DOM (if children param is specified, then with the children appended).
@@ -33,7 +67,7 @@ export default class DrumMachine {
      * @param  {...array} children same format: ["type", {props}, [...children]] 
      * @returns DOM object
      */
-     createElement(type, props, ...children) {
+    createElement(type, props, ...children) {
         const createEl = document.createElement(type); 
         for (const [key, value] of Object.entries(props)) {
             createEl[key] = value;
